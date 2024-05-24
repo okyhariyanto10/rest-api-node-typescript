2e102e2c8135eddf8bb4f126988ccf18911cc912
okyhariyanto10 <okyhariyanto10@gmail.com>
2024-05-24 23:46:41 +0700

    bugfix eslintnew

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express, { Application, Response } from 'express'
 
 const app: Application = express()
-const port: Number = 4000
+const port: number = 4000
 
-app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ status: 200 })
+app.use('/health', (res: Response) => {
+  return res.status(200).send({ status: 200 })
 })
 
 app.listen(port, () => console.log(`Server is Listening on port ${port}`))
